5f0d8b62e2a6d41ed15bc7ac66e21882e16726a4
mjsanchez@york.edu <mjsanchez@york.edu>
2024-02-11 18:21:10 -0600

    npm init

--- src/index.ts
+++ src/index.ts
@@ -13,9 +13,61 @@ class Player {
 
 
     }
+
+    getPlayerInfo(): string {
+
+        return 'Name: ${this.name}, Skill: ${this.skill}, Level: ${this.level} ';
+    }
+
+    changeName(newName: string) {
+
+        this.name = newName;
+    }
+
+    changeSkill(newSkill: string) {
+
+        this.skill = newSkill;
+    }
+
+    getLevel(): number {
+
+        return this.level;
+    }
+
+    getOrigins(origin?: string): string {
+
+        if (origin) {
+            return `The new player ${this.name}, comes from ${origin}`;
+        
+        } else {
+            return `The new player ${this.name}, comes from an unknown galaxy,`;
+        }
+    }
+    
     
 }
 
+    
 
-let user1 = new Player('Culon', 'Volar', 101)
+
+let user1 = new Player('Devorador', 'Fire Punch', 101)
 logObject(user1);
+
+user1.changeName('Aurelio');
+logObject(user1);
+
+user1.changeSkill('Swimming');
+logObject(user1);
+
+let user2 = new Player('Devo','Eat', 200);
+console.log('User2 is level:',user2.getLevel());
+
+let user3 = new Player('Rascador','Nature', 22);
+console.log(user3.getOrigins());
+console.log(user3.getOrigins('Galaxy #303'));
+
+
+
+
+
+
